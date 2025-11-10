8ca7e9df72f9612208f138f64d85ab9f92f54292
sramirezsicarony <sramirezsicarony@gmail.com>
2025-11-09 23:02:37 -0500

    Routing to create accommodation and view those
    # Conflicts:
    #	src/app/app-routing-module.ts

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -13,8 +13,10 @@ import {MyReservations}  from './pages/my-reservations/my-reservations';
 import {ConfigurationAccount} from './pages/configuration-account/configuration-account';
 import {MyChats} from './pages/my-chats/my-chats';
 import {PaymentMethod} from './pages/payment-method/payment-method';
+import {ControlPanel} from './pages/control-panel/control-panel';
+import {CreateAccomodation} from './pages/create-accomodation/create-accomodation';
+import {MyAccomodations} from './pages/my-accomodations/my-accomodations';
 import {NgModule} from '@angular/core';
-import { CreateAccomodation } from './pages/create-accomodation/create-accomodation';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
@@ -32,7 +34,9 @@ export const routes: Routes = [
   {path: 'my-chats', component: MyChats},
   {path: 'payment-method', component: PaymentMethod},
   {path: 'configuration-account', component: ConfigurationAccount},
-  {path: 'create-accommodation', component: CreateAccomodation}
+  {path: 'control-panel', component: ControlPanel},
+  {path: 'create-accomodation', component: CreateAccomodation},
+  {path: 'my-accomodations', component: MyAccomodations},
 
 
 
